3a262ed73e7e83d3f177dce6232a6394f62f2c93
Maximiliano Frega <maxifrega21@gmail.com>
2023-02-22 18:25:48 -0300

    dist y otros

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/ingeray_frontend/' : '/'
-}
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/ingefrontclon/'
+      : '/'
+  }
