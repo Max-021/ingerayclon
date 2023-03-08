2fedd46e85f4d11c7f08454b0ddc64092500ab1e
Maximiliano Frega <maxifrega21@gmail.com>
2023-03-08 11:31:57 -0300

    cambios

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env sh
-
 # abort on errors
 set -e
-
 # build
 npm run build
-
 # navigate into the build output directory
 cd dist
-
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com:Max-021/ingefrontclon.git master:gh-pages
-
+git push -f git@github.com:achani2497/ingeray_frontend.git master:gh-pages
 cd -
+clear
+echo "Live on: https://achani2497.github.io/ingeray_frontend/"
